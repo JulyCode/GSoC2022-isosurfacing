14d28d0d5cc417b086e4ff9c9dada7ff7d22b2fb
Julian Stahl <julian.stahl@fau.de>
2022-07-03 18:06:31 +0200

    Fix clock

--- src/util.h
+++ src/util.h
@@ -4,16 +4,17 @@
 #include <chrono>
 
 class ScopeTimer {
-    using TimePoint = std::chrono::_V2::system_clock::time_point;
+    using Clock = std::chrono::steady_clock;
+    using TimePoint = Clock::time_point;
 public:
-    ScopeTimer() : start(std::chrono::high_resolution_clock::now()), msg("Duration") {}
+    ScopeTimer() : start(Clock::now()), msg("Duration") {}
 
-    explicit ScopeTimer(const std::string& msg) : start(std::chrono::high_resolution_clock::now()), msg(msg) {
+    explicit ScopeTimer(const std::string& msg) : start(Clock::now()), msg(msg) {
         std::cout << msg << "..." << std::endl;
     }
 
     ~ScopeTimer() {
-        TimePoint end = std::chrono::high_resolution_clock::now();
+        TimePoint end = Clock::now();
         int64_t duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
         std::cout << msg << ": " << duration << " ms" << std::endl;
     }
