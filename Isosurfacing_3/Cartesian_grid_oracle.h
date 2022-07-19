ca4808901c52a942483c3a16a3cb6b5697b2b6af
Julian Stahl <julian.stahl@fau.de>
2022-07-19 20:58:49 +0200

    Implement oracles

---
+++ Isosurfacing_3/Cartesian_grid_oracle.h
@@ -0,0 +1,46 @@
+#ifndef CGAL_CARTESIAN_GRID_ORACLE_H
+#define CGAL_CARTESIAN_GRID_ORACLE_H
+
+#include "Cartesian_grid_3.h"
+
+namespace CGAL {
+
+template <class GeomTraits>
+class Cartesian_grid_oracle {
+public:
+    typedef GeomTraits Geom_traits;
+    typedef typename Geom_traits::FT FT;
+    typedef typename Geom_traits::Point_3 Point_3;
+
+public:
+    Cartesian_grid_oracle(const Cartesian_grid_3<Geom_traits>& grid) : grid(&grid) {}
+
+    std::size_t size_x() const {
+        return grid->xdim();
+    }
+    std::size_t size_y() const {
+        return grid->ydim();
+    }
+    std::size_t size_z() const {
+        return grid->zdim();
+    }
+
+    Point_3 position(const std::size_t x, const std::size_t y, const std::size_t z) const {
+        const FT vx = grid->voxel_x();
+        const FT vy = grid->voxel_y();
+        const FT vz = grid->voxel_z();
+
+        return Point_3(x * vx + grid->offset_x(), y * vy + grid->offset_y(), z * vz + grid->offset_z());
+    }
+
+    FT value(const std::size_t x, const std::size_t y, const std::size_t z) const {
+        return grid->value(x, y, z);
+    }
+
+private:
+    const Cartesian_grid_3<Geom_traits>* grid;
+};
+
+}  // namespace CGAL
+
+#endif  // CGAL_CARTESIAN_GRID_ORACLE_H
---
+++ Isosurfacing_3/main.cpp
@@ -0,0 +1,58 @@
+#include <CGAL/Polygon_mesh_processing/polygon_soup_to_polygon_mesh.h>
+#include <CGAL/Simple_cartesian.h>
+#include <CGAL/Surface_mesh.h>
+#include <CGAL/boost/graph/IO/OFF.h>
+#include <math.h>
+
+#include <fstream>
+#include <iostream>
+
+#include "Cartesian_grid_3.h"
+#include "Cartesian_grid_oracle.h"
+#include "Function_oracle.h"
+#include "Marching_cubes_3.h"
+
+typedef CGAL::Simple_cartesian<float> Kernel;
+typedef typename Kernel::Vector_3 Vector_3;
+typedef typename Kernel::Point_3 Point_3;
+
+typedef CGAL::Surface_mesh<Point_3> Mesh;
+typedef CGAL::Cartesian_grid_3<Kernel> Grid;
+
+typedef std::vector<Point_3> Point_range;
+typedef std::vector<std::vector<std::size_t>> Polygon_range;
+
+int main() {
+    auto sphere_function = [](const Point_3& point) {
+        return std::sqrt(point.x() * point.x() + point.y() * point.y() + point.z() * point.z());
+    };
+    CGAL::Function_oracle<Kernel, decltype(sphere_function)> sphere_oracle(sphere_function, {-1, -1, -1, 1, 1, 1},
+                                                                           Vector_3(0.002f, 0.002f, 0.002f));
+
+
+    Grid grid(sphere_oracle.size_x(), sphere_oracle.size_y(), sphere_oracle.size_z(), {-1, -1, -1, 1, 1, 1});
+
+    for (std::size_t x = 0; x < grid.xdim(); x++) {
+        for (std::size_t y = 0; y < grid.ydim(); y++) {
+            for (std::size_t z = 0; z < grid.zdim(); z++) {
+
+                grid.value(x, y, z) = sphere_oracle.value(x, y, z);
+            }
+        }
+    }
+    CGAL::Cartesian_grid_oracle<Kernel> grid_oracle(grid);
+
+    Point_range points;
+    Polygon_range polygons;
+
+    CGAL::make_triangle_mesh_using_marching_cubes(grid_oracle, 0.8f, points, polygons);
+
+    // TODO: compare results with mesh_3
+
+    Mesh mesh;
+    CGAL::Polygon_mesh_processing::polygon_soup_to_polygon_mesh(points, polygons, mesh);
+
+    CGAL::IO::write_OFF("result.off", mesh);
+
+    // write_off("result.off", result);
+}
